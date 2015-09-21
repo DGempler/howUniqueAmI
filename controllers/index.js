3b232997fad2fd30d3bfdcf0272460b1ea2e829d
Dmitry Gempler <DGempler@gmail.com>
2015-09-21 15:34:23 -0700

    set up index controllers

--- controllers/index.js
+++ controllers/index.js
@@ -4,4 +4,28 @@ var loginMiddleware = require('../middleware/loginHelper.js');
 app.use(loginMiddleware);
 
 //using locals
+app.use(function(req, res, next) {
+  if (req.session.id) {
+    db.User.findById(req.session.id, function(err, user) {
+      if (err) throw err;
+      res.locals.user = user._id;
+      next();
+    });
+  }
+  else {
+    res.locals.user = null;
+    next();
+  }
+});
 
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+require('./users');
+require('./questions');
+require('./answers');
+
+app.get('*', function(req, res) {
+  res.render('errors/404');
+});
