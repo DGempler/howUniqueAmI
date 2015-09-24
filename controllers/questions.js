434492f4289630a9e7bb089cade1c947831ea990
Dmitry Gempler <DGempler@gmail.com>
2015-09-24 09:40:06 -0700

    removed console log questions

--- controllers/questions.js
+++ controllers/questions.js
@@ -5,7 +5,6 @@ app.get('/questions/:id', routeMiddleware.ensureLoggedIn, function(req, res) {
   // if (!res.locals.user) {}
   db.Question.findOne({qID: Number(req.params.id)}, function(err, question) {
     if (err) throw err;
-    console.log(question);
     res.json(question);
   });
 });
