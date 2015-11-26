2e6237739ba6281e87e19066895030ed250aab16
Dmitry Gempler <DGempler@gmail.com>
2015-11-26 10:29:29 -0800

    removed location question from question model seed array

--- models/question.js
+++ models/question.js
@@ -19,9 +19,7 @@ var questionsArray = [
   {qID: 2, resultContent: "I was born on ", questionContent: "Enter your birthday: ", input: "<input class='date-input' id='birthday-input' type='date' autofocus/>"},
   {qID: 3, resultContent: "I am a ", questionContent: "Select your gender: ", input: "<select id='gender-select' name='answer[gender]'><option value='' disabled selected>Choose your option</option>" +
   "<option value='male'>Male</option><option value='female'>Female</option></select>"},
-  {qID: 4, resultContent: "I live in ", questionContent: "Enter your zip code: ",
-    input: "<input class='text-input' id='location-input' type='text' name='answer[location]' placeholder='00000' maxlength='5' autofocus/>"},
-  {qID: 5, resultContent: "I am ", questionContent: "Select your race:", input: "<select id='race-select' name='answer[race]'>" +
+  {qID: 4, resultContent: "I am ", questionContent: "Select your race:", input: "<select id='race-select' name='answer[race]'>" +
     "<option value='' disabled selected>Choose your option</option>" +
     "<option value='native'>American Indian or Alaska Native</option>" +
     "<option value='asian'>Asian</option>" +
@@ -32,7 +30,7 @@ var questionsArray = [
     "<option value='other'>Other</option>" +
     "<option value='two'>Two or more races</option>" +
     "</select>"},
-  {qID: 6, resultContent: "I was born in ", questionContent: "Select your place of birth: ",
+  {qID: 5, resultContent: "I was born in ", questionContent: "Select your place of birth: ",
     input: "<select id='born-select' name='answer[born]'>" +
     "<option value='' disabled selected>Choose your option</option>" +
     "<option value='europe'>The United States</option>" +
@@ -43,7 +41,7 @@ var questionsArray = [
     "<option value='latin-america'>Latin America</option>" +
     "<option value='north-america'>Other North America</option>" +
     "</select>"},
-  {qID: 7, resultContent: "At home I speak ", questionContent: "Select which language you speak at home:",
+  {qID: 6, resultContent: "At home I speak ", questionContent: "Select which language you speak at home:",
     input: "<select id='language-select' name='answer[language]'>" +
     "<option value='' disabled selected>Choose your option</option>" +
     "<option value='english'>English only</option>" +
@@ -52,7 +50,7 @@ var questionsArray = [
     "<option value='asian-islander'>Asian/Pacific Islander</option>" +
     "<option value='other'>Other</option>" +
     "</select>"},
-  {qID: 8, resultContent: "My education level is ", questionContent: "Select your education Level: ", input: "<select id='education-select-adult' name='answer[education]'>" +
+  {qID: 7, resultContent: "My education level is ", questionContent: "Select your education Level: ", input: "<select id='education-select-adult' name='answer[education]'>" +
     "<option value='' disabled selected>Choose your option</option>" +
     "<option value='8th-grade'>None thru 8th grade</option>" +
     "<option value='11th-grade'>9th - 12th grade, no diploma</option>" +
@@ -64,14 +62,14 @@ var questionsArray = [
     "<option value='professional'>Professional school degree</option>" +
     "<option value='doctoral'>Doctorate degree</option>" +
     "</select>"},
-  {qID: 9, resultContent: "I am currently ", questionContent: "Select your employment status: ", input: "<select id='employment-select' name='answer[employment]'>" +
+  {qID: 8, resultContent: "I am currently ", questionContent: "Select your employment status: ", input: "<select id='employment-select' name='answer[employment]'>" +
     "<option value='' disabled selected>Choose your option</option>" +
     "<option value='employed'>Employed</option>" +
     "<option value='unemployed'>Unemployed</option>" +
     "<option value='military'>Active Duty Military</option>" +
     "<option value='not-employed'>Not in the labor force</option>" +
     "</select>"},
-  {qID: 10, resultContent: "My annual household income is ", questionContent: "Select your annual household income: ",
+  {qID: 9, resultContent: "My annual household income is ", questionContent: "Select your annual household income: ",
     input: "<select id='income-select' name='answer[income]'>" +
     "<option value='' disabled selected>Choose your option</option>" +
     "<option value='under25'>Less than $25,000</option>" +
@@ -83,13 +81,13 @@ var questionsArray = [
     "<option value='150to200'>$150,000 to $199,999</option>" +
     "<option value='over200'>$200,000 or more</option>" +
     "</select>"},
-  {qID: 11, resultContent: "I currently ", questionContent: "I currently: ",
+  {qID: 10, resultContent: "I currently ", questionContent: "I currently: ",
     input: "<select id='tenure-select' name='answer[tenure]'>" +
     "<option value='' disabled selected>Choose your option</option>" +
     "<option value='own'>Own</option>" +
     "<option value='rent'>Rent</option>" +
     "</select>"},
-  {qID: 12, resultContent: "I live in a ", questionContent: "I live in a: ",
+  {qID: 11, resultContent: "I live in a ", questionContent: "I live in a: ",
     input: "<select id='housing-select-adult' name='answer[housing]'><option value='' disabled selected>Choose your option</option> " +
     "<option value='1-unit-detached'>one-family house detached from any other house</option>" +
     "<option value='1-unit-attached'>one-family house attached to one or more houses</option>" +
@@ -101,7 +99,7 @@ var questionsArray = [
     "<option value='mobile'>mobile home</option>" +
     "<option value='etc'>boat, RV, van, etc.</option>" +
     "</select>"},
-  {qID: 13, resultContent: "My marital status is ", questionContent: "Select your marital status", input: "<select id='marital-status-select' name='answer[maritalStatus]'>" +
+  {qID: 12, resultContent: "My marital status is ", questionContent: "Select your marital status", input: "<select id='marital-status-select' name='answer[maritalStatus]'>" +
     "<option value='' disabled selected>Choose your option</option>" +
     "<option value='never-married'>Never Married</option>" +
     "<option value='married'>Currently Married</option>" +
