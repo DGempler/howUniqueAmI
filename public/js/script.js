e9846a21712c026989be2139ad89fe1d4d8837a5
Dmitry Gempler <DGempler@gmail.com>
2015-11-26 10:41:48 -0800

    fixed questionIndex check for last question number

--- public/js/script.js
+++ public/js/script.js
@@ -47,7 +47,7 @@ $(function() {
       if ($select.length) {
         $select.material_select();
       }
-      if (questionIndex === 12) {
+      if (questionIndex === 13) {
         $indexBanner.find('#next-button').text('Submit');
         $indexBanner.find('#skip-button').text('Skip & Submit').attr('id', 'skip-submit-button');
       }
