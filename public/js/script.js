df2895baaba1e0959d1038462875e9aca29b225d
Dmitry Gempler <DGempler@gmail.com>
2016-10-29 08:21:07 -0700

    added initial webpack files

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-$(function() {
+var script = $(function() {
 
   var totalUniqueResult = {1:1, 2:1, 3:1, 4:1, 5:1, 6:1, 7:1, 8:1, 9:1, 10:1, 11:1};
   var qLinks = {1: "birthday", 2: "gender", 3: "race", 4: "place of birth", 5: "language",
@@ -713,3 +713,5 @@ $(function() {
     e.preventDefault();
   });
 });
+
+module.exports = script;
