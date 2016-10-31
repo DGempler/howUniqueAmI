b81a5bca356c5b0ef25d03c3f4c948de4c38baa1
Dmitry Gempler <DGempler@gmail.com>
2016-10-31 16:27:24 -0700

    moved a couple more result methods

--- public/js/result.js
+++ public/js/result.js
@@ -1,7 +1,43 @@
 var result = {
+  compareData: compareData,
+  compareUserAgePopToTotalPop: compareUserAgePopToTotalPop,
   getResults: getResults,
-  multiplyResult: multiplyResult
+  multiplyResult: multiplyResult,
+  showTotalUniqueResult: showTotalUniqueResult
 };
+
+function compareData(id, answer, chosenTypePop, totalPop, type, text) {
+  var singleUniqueResult = chosenTypePop / totalPop;
+  totalUniqueResult[id] = singleUniqueResult;
+  var modAnswer = modifyAnswerGrammar(answer, type);
+  var $displayedResult = $('#qId' + id).find('.single-unique-result');
+  if ($displayedResult.length === 0) {
+    $('#qId' + id).append('<h5 class="single-unique-result header col s12 light">' +
+                          (singleUniqueResult * 100).toFixed(2) + text + modAnswer + '.</h5>');
+  }
+  else {
+    $displayedResult.html((singleUniqueResult * 100).toFixed(2) + text + modAnswer + '.');
+  }
+  showTotalUniqueResult();
+}
+
+function compareUserAgePopToTotalPop(data1, data2, id, answer) {
+  var userAgePop = data1[0].total;
+  var totalPop = data2.total_population.population;
+  var singleUniqueResult = userAgePop / totalPop;
+  totalUniqueResult[id] = singleUniqueResult;
+  var $displayedResult = $('#qId' + id).find('.single-unique-result');
+  if ($displayedResult.length === 0) {
+    $('#qId' + id).append('<h5 class="single-unique-result header col s12 light">Only ' +
+                          (singleUniqueResult * 100).toFixed(2) + '% of the US Population is ' +
+                          answer + ' years old!</h5>');
+  }
+  else {
+    $displayedResult.html((singleUniqueResult * 100).toFixed(2) + '% of the US Population is ' +
+                          answer + ' years old!');
+  }
+  showTotalUniqueResult();
+}
 
 function getResults(e) {
   if (e) {
@@ -20,6 +56,21 @@ function multiplyResult() {
     multipliedResult *= totalUniqueResult[qID];
   }
   return multipliedResult;
+}
+
+function showTotalUniqueResult(numAnswers) {
+  var multipliedResult = multiplyResult();
+  $.getJSON(library.totalPop.url).done(function(data) {
+    var totalPop = data.data['01000US'].B01001.estimate.B01001001;
+    var numResult = (multipliedResult * totalPop);
+    var stringResult = numResult.toLocaleString('en-IN', { minimumFractionDigits: 2 });
+    if ($('#result-div').length === 0) {
+      addTotalUniqueResultToDOM(multipliedResult, stringResult, numAnswers);
+    }
+    else {
+      updateTotalUniqueResultOnDom(multipliedResult, stringResult);
+    }
+  });
 }
 
 module.exports = result;
--- public/js/script.js
+++ public/js/script.js
@@ -101,40 +101,6 @@ var script = $(function() {
     }
   }
 
-  function compareData(id, answer, chosenTypePop, totalPop, type, text) {
-    var singleUniqueResult = chosenTypePop / totalPop;
-    totalUniqueResult[id] = singleUniqueResult;
-    var modAnswer = modifyAnswerGrammar(answer, type);
-    var $displayedResult = $('#qId' + id).find('.single-unique-result');
-    if ($displayedResult.length === 0) {
-      $('#qId' + id).append('<h5 class="single-unique-result header col s12 light">' +
-                            (singleUniqueResult * 100).toFixed(2) + text + modAnswer + '.</h5>');
-    }
-    else {
-      $displayedResult.html((singleUniqueResult * 100).toFixed(2) + text + modAnswer + '.');
-    }
-    showTotalUniqueResult();
-  }
-
-  function compareUserAgePopToTotalPop(data1, data2, id, answer) {
-    var userAgePop = data1[0].total;
-    var totalPop = data2.total_population.population;
-    var singleUniqueResult = userAgePop / totalPop;
-    totalUniqueResult[id] = singleUniqueResult;
-    var $displayedResult = $('#qId' + id).find('.single-unique-result');
-    if ($displayedResult.length === 0) {
-      $('#qId' + id).append('<h5 class="single-unique-result header col s12 light">Only ' +
-                            (singleUniqueResult * 100).toFixed(2) + '% of the US Population is ' +
-                            answer + ' years old!</h5>');
-    }
-    else {
-      $displayedResult.html((singleUniqueResult * 100).toFixed(2) + '% of the US Population is ' +
-                            answer + ' years old!');
-    }
-    showTotalUniqueResult();
-  }
-
-
   function addTotalUniqueResultToDOM(multipliedResult, stringResult, numAnswers) {
     var $div = $('<div id="result-div" class="row center"></div');
     $indexBanner.append($div);
@@ -158,20 +124,7 @@ var script = $(function() {
     $resultDiv.find('#result-num').text(stringResult);
   }
 
-  function showTotalUniqueResult(numAnswers) {
-    var multipliedResult = multiplyResult();
-    $.getJSON(library.totalPop.url).done(function(data) {
-      var totalPop = data.data['01000US'].B01001.estimate.B01001001;
-      var numResult = (multipliedResult * totalPop);
-      var stringResult = numResult.toLocaleString('en-IN', { minimumFractionDigits: 2 });
-      if ($('#result-div').length === 0) {
-        addTotalUniqueResultToDOM(multipliedResult, stringResult, numAnswers);
-      }
-      else {
-        updateTotalUniqueResultOnDom(multipliedResult, stringResult);
-      }
-    });
-  }
+
 
   function changeFormType(oldType, newType, oldText, newText) {
     var $form = $dropdown1.find('#' + oldType + '-form');
