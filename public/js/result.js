fb4b3e45069170efa99d7e824024c63d4cb3ff60
Dmitry Gempler <dgempler@qw-corp.com>
2016-11-01 14:51:28 -0700

    moved qLinks and totalUniqueResult to respective modules, make public/private?

--- public/js/result.js
+++ public/js/result.js
@@ -8,6 +8,8 @@ var result = {
   showTotalUniqueResult: showTotalUniqueResult
 };
 
+var totalUniqueResult = {1:1, 2:1, 3:1, 4:1, 5:1, 6:1, 7:1, 8:1, 9:1, 10:1, 11:1};
+
 function compareData(id, answer, chosenTypePop, totalPop, type, text) {
   var singleUniqueResult = chosenTypePop / totalPop;
   totalUniqueResult[id] = singleUniqueResult;
--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,5 @@
 var script = $(function() {
 
-  var totalUniqueResult = {1:1, 2:1, 3:1, 4:1, 5:1, 6:1, 7:1, 8:1, 9:1, 10:1, 11:1};
-  var qLinks = {1: "birthday", 2: "gender", 3: "race", 4: "place of birth", 5: "language",
-                6: "education", 7: "employment", 8: "income", 9: "tenure", 10: "house type",
-                11: "marital status"};
-
-  // not used anywhere?
-  // var $body = $('body');
-
-  var $nav = $('nav');
-  var $indexBanner = $('#index-banner');
-  var $dropdown1 = $('#dropdown1');
-  var $dropdownButton = $nav.find('.dropdown-button');
-  var $dropdownText = $dropdownButton.find('#dropdown-text');
-  var $questionLinks =$('#question-links');
-  var questionIndex;
-  var passwordCheck;
-
 });
 
 module.exports = script;
