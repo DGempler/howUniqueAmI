417a0c17ba05fc3174a365e3706bbb6459cfb454
Dmitry Gempler <dgempler@qw-corp.com>
2016-11-01 15:02:21 -0700

    adjusted app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,11 @@
 var dom = require('./dom');
 var initEventHandlers = require('initEventHandlers');
-var script = require('./script');
-
-initEventHandlers();
-
 
 $('.button-collapse').sideNav();
 
 dom.$dropdownButton.dropdown({
-  constrain_width: false,
-  belowOrigin: true
+    constrain_width: false,
+    belowOrigin: true
 });
+
+initEventHandlers();
